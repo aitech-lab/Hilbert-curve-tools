e6077ca18ee8ffd9005599dd70688d56f8e9ce75
peko <gasu.basu@gmail.com>
2018-04-23 18:32:37 +0300

    up

---
+++ rkn/CMakeFiles/hilbert.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hilbert.dir/hilbert.c.o"
+  "hilbert.pdb"
+  "hilbert"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hilbert.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
